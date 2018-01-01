29033544473e85eae0a56044a58e80bc76c971eb
Ashley Langepfeffer <alangepfeffer@gmail.com>
2018-01-01 15:00:49 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -13,13 +13,13 @@ describe('whisper(string)', function() {
 
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
+   const spy = expect.spyOn(console, 'log').andCallThrough()
 
     logShout('hello')
 
     expect(spy).toHaveBeenCalledWith('HELLO')
 
-    console.log.restore()
+    console.log.restore()co
   })
 })
 
